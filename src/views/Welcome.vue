e1dc9a4efb734e6205810b036257bdd0a068eb23
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-28 11:00:31 +0100

    feat(plugin): testing axios: npm install axios@0.21.0 --save

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,35 @@
 <template>
-<div class='welcome'>
-  <el-button type="primary" icon="el-icon-edit"></el-button>
-  <el-button type="primary" icon="el-icon-share"></el-button>
-  <el-button type="primary" icon="el-icon-delete"></el-button>
-  <el-button type="primary" icon="el-icon-search">Search</el-button>
-  <el-button type="primary">Upload<i class="el-icon-upload el-icon-right"></i></el-button>
-</div>
+  <div class='welcome'>
+    <el-button @click='testAxios'  type='primary' icon='el-icon-edit'> Testing Axios</el-button>
+    <el-button @click='resetInfo'  type='primary' icon='el-icon-edit'> Reset</el-button>
+    <el-button type='primary' icon='el-icon-share'></el-button>
+    <el-button type='primary' icon='el-icon-delete'></el-button>
+    <el-button type='primary' icon='el-icon-search'>Search</el-button>
+    <el-button type='primary'>Upload<i class='el-icon-upload el-icon-right'></i></el-button>
+    <br>
+    {{ info }}
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Welcome'
+  name: 'Welcome',
+  data() {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    testAxios() {
+      axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
+    },
+    resetInfo() {
+      this.info = null
+    }
+  }
 }
 </script>
 
