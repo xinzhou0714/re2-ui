abbd8153620b85099a98e6b465ece282e03698e8
xinzhou0714 <zhouxin0714@gmail.com>
2022-03-11 22:47:51 +0100

    refactor(UI): refractor footer and header with components

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,41 +1,27 @@
 <template>
   <div class="welcome">
     <el-container>
-      <el-header>
-        <el-col :span="12" class="left-panel">
-          <img src="../assets/TU-Berlin-Logo.svg" alt="Logo" />
-        </el-col>
-        <el-col :span="12" class="right-panel">
-          <Icon icon="bxs:bell" width="30px" height="30px" />
-          <Icon
-            icon="ant-design:fullscreen-outlined"
-            width="30px"
-            height="30px"
-          />
-          <Icon icon="fa:language" width="30px" height="30px" />
-          <el-avatar :src="user.avatar_url"></el-avatar>
-        </el-col>
-      </el-header>
+      <el-header> <HeadBar></HeadBar> </el-header>
       <el-main>
         <Project></Project>
       </el-main>
       <el-footer>
-        <span><b>Copyright </b> @Xin Zhou</span>
-        <span>{{ description }} </span>
-        <span>Version: {{ version }}</span>
+        <FootBar></FootBar>
       </el-footer>
     </el-container>
   </div>
 </template>
 
 <script>
-import { Icon } from '@iconify/vue2'
 import Project from '@/components/project/index'
+import HeadBar from '@/components/common/HeadBar'
+import FootBar from '@/components/common/FootBar'
 export default {
   name: 'Welcome',
   components: {
-    Icon,
-    Project
+    Project,
+    HeadBar,
+    FootBar
   },
   data() {
     return {
@@ -59,48 +45,17 @@ export default {
 <style lang="scss" scoped>
 .welcome {
   height: 100%;
-  .el-container {
+  * {
     padding: 0;
     margin: 0;
+  }
+  .el-container {
     height: 100%;
 
-    .el-header {
-      background-color: #f0f0f0;
-      color: #333;
-      text-align: center;
-      line-height: 60px;
-      .left-panel {
-        display: flex;
-        align-items: center;
-        justify-items: center;
-        justify-content: flex-start;
-        height: 100%; // related to align-items: center;
-        img {
-          width: 60px;
-        }
-      }
-      .right-panel {
-        display: flex;
-        align-items: center;
-        justify-items: center;
-        justify-content: flex-end;
-        height: 100%; // related to align-items: center;
-      }
-    }
     .el-main {
       background-color: #b9e3f3;
       color: #333;
       padding: 0;
-    }
-    .el-footer {
-      background-color: #f0f0f0;
-      color: #333;
-      text-align: center;
-      line-height: 60px;
-      display: flex;
-      align-items: center;
-      justify-items: center;
-      justify-content: space-between;
     }
   }
 }
